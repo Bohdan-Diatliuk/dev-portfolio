2124564f77d984428822ba5abf7b1a24cb13cc9f
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2026-02-13 19:02:52 +0200

    setup

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="border-t">
+            <div className="container mx-auto text-center py-10">
+                <p>© {new Date().getFullYear()} Bohdan Diatliuk. Wow. Yeap buddy</p>
+            </div>
+        </footer>
+    )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <nav className="py-5 border-b">
+                <div className="container mx-auto flex justify-center items-center">
+                    <div className="text-white text-lg font-bold uppercase">Bohdan Diatliuk</div>
+                </div>
+            </nav>
+        </header>
+    );
+}
